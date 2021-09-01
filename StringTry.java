f48dfccc99def10de1de43a60406091b77ed3735
mukesh.sharmaa <mukesh.sharma0216@gmail.com>
2021-09-01 05:58:33 +0530

    StringTry Progam

---
+++ StringTry.java
@@ -0,0 +1,10 @@
+package com.Practice;
+
+import java.util.Scanner;
+
+public class StringTry {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        String s1 = in.nextLine();
+    }
+}
